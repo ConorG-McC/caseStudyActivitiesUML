6d1d70671319745ccd4a8b0e83ea6475afde532f
Conor Giffen-McCloskey <conor.giffen-mccloskey@bt.com>
2023-03-02 17:05:14 +0000

    Activity-1 Navigation Changes

--- case study/src/main/java/routers/RouteNames.java
+++ case study/src/main/java/routers/RouteNames.java
@@ -4,9 +4,16 @@ public enum RouteNames {
     HOME("home.fxml"),
     MANAGER_MENU("manager_menu.fxml"),
     STAFFUSER_MENU("staffuser_menu.fxml"),
-    ADD_MODULE("add_module.fxml"),
-    EDIT_MODULE("edit_module.fxml"),
-    SHOW_MODULES("show_modules.fxml");
+
+    VIEW_STAFF("view_staff.fxml"),
+    ADD_EDIT_STAFF("add_edit_staff.fxml"),
+
+    VIEW_SKILLS("view_skills.fxml"),
+    ADD_EDIT_DELETE_SKILLS("add_edit_delete_skills.fxml"),
+
+    VIEW_USER_DETAILS("view_user_details.fxml"),
+    EDIT_USER_DETAILS("edit_user_details.fxml");
+
 
     public final String location;
     private final String VIEW_PATH = "/views/";
